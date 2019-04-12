676a886bf80a1a51b77fa8d3d27a0aaea7b147de
lee.yongsung <lee.yongsung@jtbc.co.kr>
2019-04-12 14:33:36 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+// const babel = require('gulp-babel');
+// const eslint = require('gulp-eslint');
+
+gulp.task('default', async function() {
+
+    // gulp.src("es6/**/*.js")
+    //     .pipe(babel())
+    //     .pipe(gulp.dest("dist"));
+
+    // gulp.src("public/es6/**/*.js")
+    //     .pipe(babel())
+    //     .pipe(gulp.dest("public/dist"));
+
+});
---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    'use strict';
+    console.log('main.js loaded');
+
+});
+

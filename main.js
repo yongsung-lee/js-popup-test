b5028ceac94c4a07e8811684f626a471a0734cb1
lee.yongsung <lee.yongsung@jtbc.co.kr>
2019-04-18 18:05:08 +0900

    테스트

--- main.js
+++ main.js
@@ -2,5 +2,18 @@ $(document).ready(function () {
     'use strict';
     console.log('main.js loaded');
 
+    /*
+    $.ajax({
+        dataType: "json",
+        url: 'http://localhost:8081/opi/youtube/videos/UCsU-I-vHLiaMfV_ceaYz5rQ',
+        data: {},
+        success: function (data) {
+            $.each( data, function( key, val ) {
+                var text = '<div>' + JSON.stringify(val) + '</div>';
+                $('#youtubeData').append($(text));
+            });
+        }
+    });
+    */
 });
 

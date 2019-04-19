bd479da85f431eeb48d3fd93dc0b41a89db3446e
lee.yongsung <lee.yongsung@jtbc.co.kr>
2019-04-19 16:50:39 +0900

    카드 구성 후 정렬 까지

--- main.js
+++ main.js
@@ -2,18 +2,37 @@ $(document).ready(function () {
     'use strict';
     console.log('main.js loaded');
 
-    /*
     $.ajax({
         dataType: "json",
         url: 'http://localhost:8081/opi/youtube/videos/UCsU-I-vHLiaMfV_ceaYz5rQ',
         data: {},
         success: function (data) {
-            $.each( data, function( key, val ) {
-                var text = '<div>' + JSON.stringify(val) + '</div>';
-                $('#youtubeData').append($(text));
+            $.each( data, function( index, val ) {
+                let $gridColDiv = $('#gridCol' + index);
+                let $cardDiv = $('<div>');
+
+                $cardDiv.addClass('card');
+
+                let $thumb = $('<img>');
+                $thumb.attr('src', val.thumbnail);
+                $thumb.attr('width', 200);
+                $thumb.attr('height', 150);
+
+                let $title = $('<div>');
+                $title.html(val.title);
+                $title.addClass('title');
+
+                let $channelTitle = $('<div>');
+                $channelTitle.html(val.channel + ' <i class="fas fa-check-circle"></i>');
+                $channelTitle.addClass('channelTitle');
+
+                $cardDiv.append($thumb);
+                $cardDiv.append($title);
+                $cardDiv.append($channelTitle);
+
+                $gridColDiv.append($cardDiv);
             });
         }
     });
-    */
 });
 
